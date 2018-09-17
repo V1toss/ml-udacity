d5f934b0fc5d17f6907637d5a77480182ebb45cd
victor.karetko <victor.karetko@octopod.com>
2018-09-17 23:37:21 +0300

    explore enron

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,30 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print len(enron_data)
 
+print min([len(v) for v in enron_data.itervalues()])
+
+print sum([v['poi'] for v in enron_data.itervalues()])
+
+print sum(1 for line in open("../final_project/poi_names.txt", "r") if line.startswith("("))
+
+print enron_data['PRENTICE JAMES']['total_stock_value']
+
+print enron_data['COLWELL WESLEY']['from_this_person_to_poi']
+
+print enron_data['SKILLING JEFFREY K']['exercised_stock_options']
+
+mykeys = ['SKILLING JEFFREY K', 'LAY KENNETH L', 'FASTOW ANDREW S']
+print [(k, v['total_payments']) for k, v in enron_data.iteritems() if k in mykeys]
+
+print sum([v['salary'] != 'NaN' for v in enron_data.itervalues()])
+print sum([v['email_address'] != 'NaN' for v in enron_data.itervalues()])
+
+print (sum([v['total_payments'] == 'NaN' and v['poi'] for v in enron_data.itervalues()]) / float(len(enron_data)))
+
+print (sum([v['total_payments'] == 'NaN' for v in enron_data.itervalues()])) + 10
+
+print sum([v['poi'] for v in enron_data.itervalues()]) + 10
+
+print (sum([v['total_payments'] == 'NaN' and v['poi'] for v in enron_data.itervalues()])) + 10
